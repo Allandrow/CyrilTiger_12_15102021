85bb537af392d4d806df4a706f851d0c0eafd2d2
Cyril Tiger <cyriltiger@gmail.com>
2021-10-18 17:09:17 +0200

    end header

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,11 +1,13 @@
+import Navigation from '../navigation/navigation'
 import logo from './logo.svg'
 
 const Header = () => {
   return (
-    <header className="bg-dark">
+    <header className="bg-dark flex justify-between gap-36 items-center p-5">
       <h1>
         <img src={logo} alt="SportSee" />
       </h1>
+      <Navigation />
     </header>
   )
 }
---
+++ src/components/navigation/navigation.jsx
@@ -0,0 +1,22 @@
+const Navigation = () => {
+  return (
+    <nav className="text-white text-2xl font-medium flex-1">
+      <ul className="flex justify-between">
+        <li>
+          <a href="">Accueil</a>
+        </li>
+        <li>
+          <a href="">Profil</a>
+        </li>
+        <li>
+          <a href="">Réglage</a>
+        </li>
+        <li>
+          <a href="">Communauté</a>
+        </li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Navigation
