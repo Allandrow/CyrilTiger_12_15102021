59f8eec4266c60600d6c69ccb459903e59eb0d46
Cyril Tiger <cyriltiger@gmail.com>
2021-10-18 15:38:20 +0200

    cleanup + setup tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import { useState } from 'react'
-import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
+    <div>
       <h1>App</h1>
     </div>
   )
