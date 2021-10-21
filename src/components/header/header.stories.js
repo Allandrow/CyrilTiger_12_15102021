60e824f92d756003afb2551857696c2788501010
Cyril Tiger <cyriltiger@gmail.com>
2021-10-21 15:28:26 +0200

    added Header story

---
+++ src/components/header/header.stories.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Header from './header'
+
+export default {
+  component: Header,
+  title: 'SportSee/components/Header'
+}
+
+const Template = () => <Header />
+
+export const Default = Template.bind({})
