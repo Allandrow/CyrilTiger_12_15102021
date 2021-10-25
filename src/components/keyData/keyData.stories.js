6f30b35b03a60edf1a7ddac273642f442bd311bd
Cyril Tiger <cyriltiger@gmail.com>
2021-10-25 14:32:29 +0200

    added keyData component + stories

---
+++ src/components/keyData/keyData.stories.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import KeyData from './keyData'
+
+export default {
+  component: KeyData,
+  title: 'sportsee/dashboard/keyData'
+}
+
+const Template = (args) => <KeyData {...args} />
+
+export const Calories = Template.bind({})
+Calories.args = {
+  value: 1930,
+  type: 'calorieCount'
+}
+
+export const Proteines = Template.bind({})
+Proteines.args = {
+  value: 155,
+  type: 'proteinCount'
+}
+
+export const Glucides = Template.bind({})
+Glucides.args = {
+  value: 290,
+  type: 'carbohydrateCount'
+}
+
+export const Lipides = Template.bind({})
+Lipides.args = {
+  value: 50,
+  type: 'lipidCount'
+}
