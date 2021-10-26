0472bc16677bc5dba59f883bea439539af13dca4
Cyril Tiger <cyriltiger@gmail.com>
2021-10-26 16:25:23 +0200

    added getKeyData fetch

--- src/components/keyDatas/keyDatas.jsx
+++ src/components/keyDatas/keyDatas.jsx
@@ -1,36 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import KeyData from '../keyData/keyData'
-import PropTypes from 'prop-types'
+import { getKeyData } from '../../services'
 
-/**
- * Section of key data cards
- * @param {number} calorieCount numeric value of calories
- * @param {number} proteinCount numeric value of proteins
- * @param {number} carbohydrateCount numeric value of carbohydrates
- * @param {number} lipidCount numeric value of lipids
- */
+const KeyDatas = () => {
+  const [keyData, setKeyData] = useState(null)
 
-const KeyDatas = ({
-  calorieCount,
-  proteinCount,
-  carbohydrateCount,
-  lipidCount
-}) => {
-  return (
-    <div className='flex flex-col justify-between h-full'>
-      <KeyData value={calorieCount} type="calorieCount" />
-      <KeyData value={proteinCount} type="proteinCount" />
-      <KeyData value={carbohydrateCount} type="carbohydrateCount" />
-      <KeyData value={lipidCount} type="lipidCount" />
-    </div>
-  )
-}
+  useEffect(() => {
+    getKeyData(setKeyData)
+  }, [])
 
-KeyDatas.propTypes = {
-  calorieCount: PropTypes.number.isRequired,
-  proteinCount: PropTypes.number.isRequired,
-  carbohydrateCount: PropTypes.number.isRequired,
-  lipidCount: PropTypes.number.isRequired
+  if (keyData) {
+    return (
+      <div className="flex flex-col justify-between h-full">
+        <KeyData value={keyData.calorieCount} type="calorieCount" />
+        <KeyData value={keyData.proteinCount} type="proteinCount" />
+        <KeyData value={keyData.carbohydrateCount} type="carbohydrateCount" />
+        <KeyData value={keyData.lipidCount} type="lipidCount" />
+      </div>
+    )
+  } else {
+    return <div>Loading</div>
+  }
 }
 
 export default KeyDatas
--- src/services.js
+++ src/services.js
@@ -1,11 +1,22 @@
 // service functions fetching mock data
 import axios from 'axios'
 
+const userInfosURL = '../src/data/userInfos.json'
+
 export const getUserName = (cb) => {
   axios
-    .get('../src/data/userInfos.json')
+    .get(userInfosURL)
     .then((res) => {
       cb(res.data.userInfos.firstName)
     })
-    .catch((err) => console.log(err))
+    .catch((err) => console.error(err))
+}
+
+export const getKeyData = (cb) => {
+  axios
+    .get(userInfosURL)
+    .then((res) => {
+      cb(res.data.keyData)
+    })
+    .catch((err) => console.error(err))
 }
