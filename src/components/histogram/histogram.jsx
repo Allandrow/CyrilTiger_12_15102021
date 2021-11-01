7471647685b68e73056cf1d1fbd3f34d5abf7c4b
Cyril Tiger <cyriltiger@gmail.com>
2021-11-01 18:34:22 +0100

    WIP histogram

--- src/components/histogram/histogram.jsx
+++ src/components/histogram/histogram.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { getUserActivity } from '../../services'
+import * as d3 from 'd3'
 
 const Histogram = () => {
   const [activity, setActivity] = useState(null)
@@ -8,18 +9,40 @@ const Histogram = () => {
     getUserActivity(setActivity)
   }, [])
 
-  if (activity) {
-    const sessions = activity.map(session => <li key={session.day}>{session.day}, {session.kilogram}, {session.calories}</li>)
-    return (
-      <div>
-        <h2>Histogram</h2>
-        <p>Data :</p>
-        <ul>{sessions}</ul>
+  if (activity) makeSVG(activity)
+  return (
+    <div className="bg-gray-50 rounded-md p-4">
+      <div className="flex justify-between">
+        <h3 className="font-medium">Activité quotidienne</h3>
+        <div className="flex gap-4 font-medium text-sm text-gray-500">
+          <div className="flex items-center gap-2">
+            <span className="block w-2 h-2 bg-black rounded"></span>
+            <span>Poids (kg)</span>
+          </div>
+          <div className="flex items-center gap-2">
+            <span className="block w-2 h-2 rounded bg-red-500"></span>
+            <span>Calories brûlées (kCal)</span>
+          </div>
+        </div>
       </div>
-    )
-  } else {
-    return <div>Loading</div>
-  }
+      <div id="histogram"></div>
+    </div>
+  )
+}
+
+const makeSVG = (data) => {
+  console.log(data)
+  const width = 740
+  const height = 145
+  const margin = 30
+
+  // svg canvas
+  d3.select('#histogram')
+    .append('svg')
+    .attr('width', width + margin * 2)
+    .attr('height', height + margin * 2)
+    .append('g')
+    .attr('transform', `translate(${margin}, ${margin})`)
 }
 
 export default Histogram
