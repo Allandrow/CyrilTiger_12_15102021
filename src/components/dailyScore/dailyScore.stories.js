929acda6bcf6b96698bb26b21ac36deb5cc885ed
Cyril Tiger <cyriltiger@gmail.com>
2021-11-01 16:16:46 +0100

    added daily score story

---
+++ src/components/dailyScore/dailyScore.stories.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import DailyScore from './dailyScore'
+
+export default {
+  component: DailyScore,
+  title: 'sportsee/Dashboard/Charts/Daily-score'
+}
+
+const Template = () => <DailyScore />
+
+export const Default = Template.bind({})
