c6bfaf8f84e38aa70cfa6168be61a4671ea42764
Cyril Tiger <cyriltiger@gmail.com>
2021-11-10 16:13:11 +0100

    added custom tick in radar chart

--- src/components/performance/performance.jsx
+++ src/components/performance/performance.jsx
@@ -35,7 +35,7 @@ const Performance = () => {
       <ResponsiveContainer width="100%" height="100%">
         <RadarChart data={performance}>
           <PolarGrid />
-          <PolarAngleAxis dataKey="kind" />
+          <PolarAngleAxis dataKey="kind" tick={customTicks} />
           <PolarRadiusAxis
             angle={30}
             domain={[0, Math.max(...performance.map((item) => item.value))]}
@@ -45,6 +45,31 @@ const Performance = () => {
         </RadarChart>
       </ResponsiveContainer>
     </div>
+  )
+}
+
+const customTicks = (data) => {
+  return (
+    <text
+      type={data.type}
+      cx={data.cx}
+      cy={data.cy}
+      orientation={data.orientation}
+      radius={data.radius}
+      stroke={data.stroke}
+      x={data.x}
+      y={data.y}
+      textAnchor={data.textAnchor}
+    >
+      <tspan
+        x={data.payload.coordinate.x}
+        dy={5}
+        fontSize="0.75rem"
+        className='capitalize'
+      >
+        {data.payload.value}
+      </tspan>
+    </text>
   )
 }
 
