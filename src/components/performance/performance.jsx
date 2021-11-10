7ad5e86b74bcf74dfb26c68b8969189ce8ce767d
Cyril Tiger <cyriltiger@gmail.com>
2021-11-10 15:43:15 +0100

    cleanup radarchart

--- src/components/performance/performance.jsx
+++ src/components/performance/performance.jsx
@@ -33,9 +33,9 @@ const Performance = () => {
   return (
     <div className="flex-1 rounded bg-secondary p-4 fill-current text-white">
       <ResponsiveContainer width="100%" height="100%">
-        <RadarChart data={performance} outerRadius={80}>
+        <RadarChart data={performance}>
           <PolarGrid />
-          <PolarAngleAxis dataKey="kind" tickLine={false} />
+          <PolarAngleAxis dataKey="kind" />
           <PolarRadiusAxis
             angle={30}
             domain={[0, Math.max(...performance.map((item) => item.value))]}
