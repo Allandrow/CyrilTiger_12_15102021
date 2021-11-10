c4938fbad24bc91709d684143e7de36c45f9b7da
Cyril Tiger <cyriltiger@gmail.com>
2021-11-10 18:05:10 +0100

    added userRadarChart component

--- src/components/performance/performance.jsx
+++ src/components/performance/performance.jsx
@@ -1,28 +1,11 @@
 import React from 'react'
-import {
-  PolarAngleAxis,
-  PolarGrid,
-  PolarRadiusAxis,
-  Radar,
-  RadarChart,
-  ResponsiveContainer
-} from 'recharts'
 import { getUserPerformance } from '../../hooks/getUserPerformance'
 import Loader from '../loader/loader'
 import Error from '../error/error'
+import UserRadarChart from '../userRadarChart/userRadarChart'
 
 const Performance = () => {
   const { loading, error, data } = getUserPerformance()
-  let performance = []
-
-  if (data) {
-    performance = data.data.map((item) => {
-      return {
-        value: item.value,
-        kind: data.kind[item.kind]
-      }
-    })
-  }
 
   if (loading) {
     return <Loader />
@@ -34,44 +17,8 @@ const Performance = () => {
 
   return (
     <div className="flex-1 rounded bg-secondary p-4 fill-current text-white">
-      <ResponsiveContainer width="100%" height="100%">
-        <RadarChart data={performance}>
-          <PolarGrid />
-          <PolarAngleAxis dataKey="kind" tick={customTicks} />
-          <PolarRadiusAxis
-            angle={30}
-            domain={[0, Math.max(...performance.map((item) => item.value))]}
-            tick={false}
-          />
-          <Radar dataKey="value" stroke="red" fill="red" fillOpacity={0.8} />
-        </RadarChart>
-      </ResponsiveContainer>
+      <UserRadarChart kind={data.kind} data={data.data} />
     </div>
-  )
-}
-
-const customTicks = (data) => {
-  return (
-    <text
-      type={data.type}
-      cx={data.cx}
-      cy={data.cy}
-      orientation={data.orientation}
-      radius={data.radius}
-      stroke={data.stroke}
-      x={data.x}
-      y={data.y}
-      textAnchor={data.textAnchor}
-    >
-      <tspan
-        x={data.payload.coordinate.x}
-        dy={5}
-        fontSize="0.75rem"
-        className="capitalize"
-      >
-        {data.payload.value}
-      </tspan>
-    </text>
   )
 }
 
---
+++ src/components/userRadarChart/userRadarChart.jsx
@@ -0,0 +1,67 @@
+import React from 'react'
+import {
+  PolarAngleAxis,
+  PolarGrid,
+  PolarRadiusAxis,
+  Radar,
+  RadarChart,
+  ResponsiveContainer
+} from 'recharts'
+import PropTypes from 'prop-types'
+
+/**
+ * Radar Chart Component
+ * @param {Object} kind Categories
+ * @param {Array} data Values for each category
+ */
+const UserRadarChart = ({ kind, data }) => {
+  const performance = data.map((item) => {
+    return {
+      value: item.value,
+      kind: kind[item.kind]
+    }
+  })
+
+  return (
+    <ResponsiveContainer width="100%" height="100%">
+      <RadarChart data={performance}>
+        <PolarGrid />
+        <PolarAngleAxis dataKey="kind" tick={customTicks} />
+        <PolarRadiusAxis angle={30} domain={[0, 'auto']} tick={false} />
+        <Radar dataKey="value" stroke="red" fill="red" fillOpacity={0.8} />
+      </RadarChart>
+    </ResponsiveContainer>
+  )
+}
+
+const customTicks = (data) => {
+  return (
+    <text
+      type={data.type}
+      cx={data.cx}
+      cy={data.cy}
+      orientation={data.orientation}
+      radius={data.radius}
+      stroke={data.stroke}
+      x={data.x}
+      y={data.y}
+      textAnchor={data.textAnchor}
+    >
+      <tspan
+        x={data.payload.coordinate.x}
+        dy={5}
+        fontSize="0.75rem"
+        className="capitalize"
+      >
+        {data.payload.value}
+      </tspan>
+    </text>
+  )
+}
+
+UserRadarChart.propTypes = {
+  kind: PropTypes.object,
+  data: PropTypes.array
+}
+
+export default UserRadarChart
