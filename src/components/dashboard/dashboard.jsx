9926f24e24a8784aaa0d18bcd24c645992bb8ce2
Cyril Tiger <cyriltiger@gmail.com>
2021-11-23 16:42:36 +0100

    modified padding for dashboard

--- src/components/dashboard/dashboard.jsx
+++ src/components/dashboard/dashboard.jsx
@@ -7,7 +7,7 @@ import UserIdProvider from '../../layout/userIdContext'
 const Dashboard = () => {
   return (
     <UserIdProvider>
-      <section className="flex p-8 flex-col flex-1 order-1 col-span-2">
+      <section className="flex p-8 pt-32 pl-32 flex-col flex-1 order-1 col-span-2">
         <Headline />
         <div className="mt-8 flex-1 grid gap-8 grid-cols-4 xxl:mt-16 xxl:grid-flow-col">
           <Charts />
